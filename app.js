5a0f950aa8df1e20f6e70168e16219997f12808a
Jasper Alblas <just_jasper@hotmail.com>
2022-10-25 16:31:37 +0200

    Register form added

--- app.js
+++ app.js
@@ -13,8 +13,10 @@ const Campground = require('./models/campground');
 const Review = require('./models/review');
 const User = require('./models/user');
 
-const campgrounds = require('./routes/campgrounds');
-const reviews = require('./routes/reviews');
+const userRoutes = require('./routes/users');
+const campgroundRoutes = require('./routes/campgrounds');
+const reviewRoutes = require('./routes/reviews');
+const user = require('./models/user');
 
 mongoose.connect('mongodb://localhost:27017/camp', {});
 
@@ -59,8 +61,9 @@ app.use((req, res, next) => {
     next();
 })
 
-app.use('/campgrounds', campgrounds);
-app.use('/campgrounds/:id/reviews', reviews);
+app.use('/', userRoutes);
+app.use('/campgrounds', campgroundRoutes);
+app.use('/campgrounds/:id/reviews', reviewRoutes);
 
 app.get('/', (req, res) => {
     res.render('home')
